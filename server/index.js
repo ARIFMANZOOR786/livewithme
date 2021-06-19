b80b15fa62afaac0e5ef62376ae5a7018a91b4cf
ARIFMANZOOR786 <arifmanzoor127@gmail.com>
2021-06-19 16:51:33 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
+const app = express();
+dotenv.config();
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb",extended: true}));
+app.use(cors());
+app.use('/posts', postRoutes);
+
+// const CONNECTION_URL ='mongodb+srv://arif_123:9697050691@cluster0.on4pc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology:true})
+.then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
